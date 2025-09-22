8116bd82693dac089e2e2427fcf8a4910d360be0
aliia <aliia.malaeva@alatoo.edu.kg>
2025-09-22 10:00:33 +0600

    Initial commit: create Spring Boot project with dependencies

---
+++ src/main/java/kg/megalab/smart_kindergarten_management/SmartKindergartenManagementApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.smart_kindergarten_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKindergartenManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKindergartenManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/megalab/smart_kindergarten_management/SmartKindergartenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.smart_kindergarten_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartKindergartenManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
