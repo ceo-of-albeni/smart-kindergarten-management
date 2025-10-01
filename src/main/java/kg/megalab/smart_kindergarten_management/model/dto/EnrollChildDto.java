382289f70dcbfad5ee29e6476c385178035ff07e
aliia <aliia.malaeva@alatoo.edu.kg>
2025-10-01 10:49:18 +0600

    Add Child entity and DTO with validation annotations

---
+++ src/main/java/kg/megalab/smart_kindergarten_management/model/dto/EnrollChildDto.java
@@ -0,0 +1,33 @@
+package kg.megalab.smart_kindergarten_management.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnrollChildDto {
+    @NotBlank(message = "Имя обязательно")
+    String firstName;
+
+    @NotBlank(message = "Фамилия обязательна")
+    String lastName;
+
+    String patronymic;
+
+    @NotNull(message = "Дата рождения обязательна")
+    LocalDate dateOfBirth;
+
+    @NotNull(message = "ID группы обязателен")
+    Long groupId;
+
+    Integer price; // если индивидуальная цена отличается от категории
+}
